0c1fc5ebf7347384468de50846f4f03ee83e9a78
Jeandre-kotze <Imonping7@gmail.com>
2024-10-17 11:11:16 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['firebase', 'firebase/firestore', 'firebase/app', 'firebase/auth'],
+      external: ['firebase', 'firebase/firestore', 'firebase/app', 'firebase/auth', 'firebase/storage'],
     }
   }  
 })
